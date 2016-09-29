7b99aeb06cf1e08e80ceff34a8bf96c9aa527ff1
Jim Dixon <jddixon@gmail.com>
2016-09-29 09:53:58 -0700

    correct license

--- src/java/org/xlattice/projmgr/Version.java
+++ src/java/org/xlattice/projmgr/Version.java
@@ -10,7 +10,7 @@ public class Version extends org.xlattice.Version {
         super("org.xlattice.projmgr", // package
             0,                       // major
             4,                       // minor
-            6,                       // decimal
+            7,                       // decimal
             0);                      // build
     }
 };
